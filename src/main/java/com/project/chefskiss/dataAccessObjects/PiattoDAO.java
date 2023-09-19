30e16565ea183d6fe310941547ca3c37759d01be
Sara <saratullini01@gmail.com>
2023-09-19 15:07:36 +0200

    piatto DAO

---
+++ src/main/java/com/project/chefskiss/dataAccessObjects/PiattoDAO.java
@@ -0,0 +1,32 @@
+package com.project.chefskiss.dataAccessObjects;
+
+import com.project.chefskiss.modelObjects.Ingrediente;
+import com.project.chefskiss.modelObjects.Piatto;
+import com.project.chefskiss.modelObjects.Sede;
+import com.project.chefskiss.modelObjects.User;
+
+import java.util.List;
+
+public interface PiattoDAO {
+    public Piatto create (
+            String Nome_Piatto,
+            String Preparazione,
+            String CF_Utente
+    );
+
+    public void update (Piatto piatto);
+
+    public void delete (Piatto piatto);
+
+    // cerca un piatto per nome
+    public List<Piatto> findByName (String Nome_Piatto);
+
+    // cerca un piatto per ingrediente
+    public List<Piatto> findByIngediente (Ingrediente Ingrediente);
+
+    // cerca un piatto per sede
+    public List<Piatto> findBySede (Sede Sede);
+
+    // cerca un piatto per CF privato/chef/recensori
+    public List<Piatto> findByCF (User CF_Utente);
+}
