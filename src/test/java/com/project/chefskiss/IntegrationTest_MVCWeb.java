1ff67e71a2733e32e565217c2844a77bc68948a4
davidenegri0 <davidenegri0@gmail.com>
2024-05-30 12:42:17 +0200

    Fix a homepageController e homepage.jsp per integration testing efficace

---
+++ src/test/java/com/project/chefskiss/IntegrationTest_MVCWeb.java
@@ -0,0 +1,36 @@
+package com.project.chefskiss;
+import com.project.chefskiss.controllers.homepageController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ContextConfiguration
+@WebAppConfiguration
+public class IntegrationTest_MVCWeb {
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new homepageController())
+                .alwaysExpect(status().isOk())
+                //.alwaysExpect(content().contentType(MediaType.TEXT_HTML))
+                .build();
+    }
+
+    @Test
+    void TestHomepageAvanzato() throws Exception{
+        mockMvc.perform(get("/homepage"))
+                .andExpectAll(
+                        status().isOk(),
+                        model().hasNoErrors(),
+                        model().attributeExists("listaPiatti")
+                );
+    }
+}
