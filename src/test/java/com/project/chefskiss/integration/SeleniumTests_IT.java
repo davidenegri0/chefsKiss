44ab4f1e38a471154f7042e4bc7cd9ba71554e4b
davidenegri0 <davidenegri0@gmail.com>
2024-06-21 20:40:06 +0200

    Provo (ma se non va fa lo stesso) la registrazione

--- src/test/java/com/project/chefskiss/integration/SeleniumTests_IT.java
+++ src/test/java/com/project/chefskiss/integration/SeleniumTests_IT.java
@@ -52,7 +52,7 @@ public class SeleniumTests_IT {
             .withCapabilities(new ChromeOptions())
             .withNetwork(webapp.getNetwork())
             .withNetworkAliases("chrome")
-            //.withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("test_video"))
+            .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("/home/runner/work/chefsKiss/chefsKiss/target/site"))
             .dependsOn(webapp);
 
     @BeforeAll
